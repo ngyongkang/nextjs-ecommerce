dc50cc42fa8d63e663e36a526bcc80e5680d0761
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-30 15:07:02 +0800

    removed NEXTAUTH_RUL from check

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,7 +8,7 @@ const envSchema = zod.object({
   DATABASE_URL: zod.string().min(1),
   GOOGLE_CLIENT_ID: zod.string().min(1),
   GOOGLE_CLIENT_SECRET: zod.string().min(1),
-  NEXTAUTH_URL: zod.string().min(1),
+  // NEXTAUTH_URL: zod.string().min(1),
   NEXTAUTH_SECRET: zod.string().min(1),
 });
 
