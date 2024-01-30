9eca598549876a7c2641fe41bf1e377c4624e899
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-30 14:59:18 +0800

    Shifted authOptions to a different folder.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,5 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { NextAuthOptions } from 'next-auth';
-import prisma from '@/lib/db/prisma';
-import { Adapter } from 'next-auth/adapters';
-import Google from 'next-auth/providers/google';
+import { authOptions } from '@/app/utils/authOptions';
 import NextAuth from 'next-auth/next';
-import { env } from '@/lib/env';
-import { User } from '@prisma/client';
-import { mergeAnonymousCartIntoUserCart } from '@/lib/db/cart';
-
-// By adding the callback section we can change the
-// default session variables. As it is in TS we need to add the type
-// as well.
-
-// By adding the events section we can modify the default functions
-// provided by the default session.
-const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    Google({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id;
-      session.user.type = (user as User).type;
-      return session;
-    },
-  },
-  events: {
-    async signIn({ user }) {
-      await mergeAnonymousCartIntoUserCart(user.id);
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 
---
+++ src/app/utils/authOptions.ts
@@ -0,0 +1,36 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { NextAuthOptions } from 'next-auth';
+import prisma from '@/lib/db/prisma';
+import { Adapter } from 'next-auth/adapters';
+import Google from 'next-auth/providers/google';
+import { env } from '@/lib/env';
+import { User } from '@prisma/client';
+import { mergeAnonymousCartIntoUserCart } from '@/lib/db/cart';
+
+// By adding the callback section we can change the
+// default session variables. As it is in TS we need to add the type
+// as well.
+
+// By adding the events section we can modify the default functions
+// provided by the default session.
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      session.user.type = (user as User).type;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonymousCartIntoUserCart(user.id);
+    },
+  },
+};
