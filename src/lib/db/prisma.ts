efcbcc8cca7bcab77977da0c65a102b6270bbbc0
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-30 11:50:52 +0800

    Addedglobal settings for the update on cart table.

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,17 +1,29 @@
 // To provide development environment a prisma client on load.
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  return new PrismaClient();
+};
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+const prismaBase = globalThis.prisma ?? prismaClientSingleton();
 
-export default prisma
+// Added this section to apply changes to the update on "cart"
+// this changes are applied globally therefore, use with caution.
+const prisma = prismaBase.$extends({
+  query: {
+    cart: {
+      async update({ args, query }) {
+        args.data = { ...args.data, updateAt: new Date() };
+        return query(args);
+      },
+    },
+  },
+});
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismaBase;
